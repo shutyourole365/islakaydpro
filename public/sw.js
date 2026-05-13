695881cf534b8799be452e629e9f70537997563e
islakayd <bungara365@gmail.com>
2026-05-13 14:29:29 +1000

    fix: remove duplicate push event listener from service worker

--- public/sw.js
+++ public/sw.js
@@ -260,44 +260,6 @@ self.addEventListener('sync', (event) => {
   }
 });
 
-// Push notification event
-self.addEventListener('push', (event) => {
-  console.log('[SW] Push received:', event);
-
-  let data = {};
-  if (event.data) {
-    data = event.data.json();
-  }
-
-  const options = {
-    body: data.body || 'You have a new notification from Islakayd',
-    icon: '/icons/icon-192x192.png',
-    badge: '/icons/icon-72x72.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: data.primaryKey || 1,
-      url: data.url || '/'
-    },
-    actions: [
-      {
-        action: 'view',
-        title: 'View',
-        icon: '/icons/icon-72x72.png'
-      },
-      {
-        action: 'dismiss',
-        title: 'Dismiss'
-      }
-    ],
-    requireInteraction: true,
-    silent: false
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(data.title || 'Islakayd', options)
-  );
-});
 
 // Notification click event
 self.addEventListener('notificationclick', (event) => {
